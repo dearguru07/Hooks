10d88f74cb909fea7b964bd54d12464de3923412
dearguru07 <pangalaguruyadav101@gmail.com>
2024-08-13 17:54:45 +0530

    'changes;

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
+// ------------------This Page for useState Hooks------------
+
 const Hooks = () => {
-  let [a,seta]=useState(0)
-  let [b,setb]=useState(10)
-  function click(){
-    setb(22)
-    console.log(b)
+  let [a, seta] = useState(0);
+  let [b, setb] = useState(10);
+
+  // varible state-------------------
+
+  function click() {
+    setb(22);
+    console.log(b);
   }
+  // returnstate----------------------
   return (
     <div>
-      <h1>Hello world.....</h1>
+      {/* ----------------method-1---------------------------- */}
+
+      <h1>useState Hook</h1>
       <h2>{a}</h2>
-      <button onClick={()=>{
-       const High=a+1
-       seta(High)
-       
-      }}>Press</button>
-      <button onClick={()=>{
-        const Low=a-1
-        seta(Low)
-      }}>Low</button>
+      <button onClick={() => {
+          const High = a + 1;
+          seta(High);
+        }}>Hiigh</button>
+
+      {/* ---------------method-2-----------------    */}
+
+      <button onClick={() => {
+          const Low = a-1;
+          seta(Low);
+        }}>Low</button>
+      
+      {/*------------------ method-3-------------------------------- */}
+
       <h1>{b}</h1>
-      <button onClick={click}>click here</button>
+      <button onClick={click}>Click here</button>
     </div>
   );
 };
